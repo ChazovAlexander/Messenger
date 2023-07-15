e01a74f7e4bdf0622bd0dc4ef78fd78b04061067
1the_boneless <cazovsanek@gmail.com>
2023-07-15 18:13:54 +0200

    Added new files

---
+++ app/src/main/java/com/example/messenger/Messenger.java
@@ -0,0 +1,15 @@
+package com.example.messenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.SearchView;
+
+import android.os.Bundle;
+
+public class Messenger extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_messenger);
+    }
+}
